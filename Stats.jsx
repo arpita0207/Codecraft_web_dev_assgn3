37e41b1111206a68c2c773e88a228a9c6263e5f8
arpita0207 <131021010+arpita0207@users.noreply.github.com>
2024-06-26 22:19:51 +0530

    Add files via upload

---
+++ Stats.jsx
@@ -0,0 +1,16 @@
+import React from "react";
+import "./Stats.css";
+
+const Stats = ({ stats }) => {
+  return (
+    <ul>
+      {Object.entries(stats).map(([key, value]) => (
+        <li key={key}>
+          {key}: {value}
+        </li>
+      ))}
+    </ul>
+  );
+};
+
+export default Stats;
